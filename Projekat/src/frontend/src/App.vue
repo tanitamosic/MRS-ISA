5c84eaabd5858d3b1cbf8cf852e14719eb65a6a3
Lazar Pavlović <lp.pavlovic.001@gmail.com>
2022-05-09 22:43:58 +0200

    NavBar povezan sa App komponentom.

--- Projekat/src/frontend/src/App.vue
+++ Projekat/src/frontend/src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <NavigationBar></NavigationBar>
+    <router-view/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavigationBar from './components/NavigationBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    NavigationBar
+}
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
