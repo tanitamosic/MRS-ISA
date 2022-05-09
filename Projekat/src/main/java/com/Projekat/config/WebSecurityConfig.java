4ebf764f6a8815e1df5844331fc39311c0a87360
Tanita Mošić <tanitamosic@gmail.com>
2022-05-09 15:29:36 +0200

    Dodate klase za konfiguraciju i autentifikaciju

---
+++ Projekat/src/main/java/com/Projekat/config/WebSecurityConfig.java
@@ -0,0 +1,72 @@
+package com.Projekat.config;
+
+import com.Projekat.security.RestAuthenticationEntryPoint;
+import com.Projekat.security.TokenAuthenticationFilter;
+import com.Projekat.service.CustomAccountDetailsService;
+import com.Projekat.util.TokenUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+// Ukljucivanje podrske za anotacije "@Pre*" i "@Post*" koje ce aktivirati autorizacione provere za svaki pristup metodi
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	// @Bean
+	// public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+
+	@Autowired
+	private CustomAccountDetailsService customUserDetailsService;
+
+	@Autowired
+	private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(customUserDetailsService)
+				.passwordEncoder(new BCryptPasswordEncoder());
+	}
+
+	@Autowired
+	private TokenUtils tokenUtils;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and()
+
+			.authorizeRequests().antMatchers("/auth/**").permitAll()
+								.antMatchers("/index").permitAll() // pocetna strana sa svim ponudama
+								.antMatchers("/admin").hasRole("ADMIN")
+								.anyRequest().authenticated().and()
+			.cors().and().addFilterBefore(new TokenAuthenticationFilter(tokenUtils, customUserDetailsService), BasicAuthenticationFilter.class);
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		 web.ignoring().antMatchers(HttpMethod.POST, "/auth/login");
+		 web.ignoring().antMatchers(HttpMethod.GET, "/", "/webjars/**", "/*.html", "favicon.ico", "/**/*.html",
+				"/**/*.css", "/**/*.js");
+	}
+
+}
