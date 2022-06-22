24e4c1e25e003e5fef11244c7d5bb8faca91f39b
Tanita Mošić <tanitamosic@gmail.com>
2022-06-22 23:59:46 +0200

    Sklonjen debugger

---
+++ Projekat/src/frontend/src/components/InstructorHistory.vue
@@ -0,0 +1,204 @@
+<template>
+    <div v-if="!this.ReservationsLoaded" id="ucitavanje">Učitavanje!</div>
+    <div v-else-if="this.ReservationsLoaded && this.ReservationsEmpty" id="nema-podataka">Nema podataka za prikaz!</div>
+    <div v-else-if="this.ReservationsLoaded && this.ReservationsEmpty === false" pb-5>
+        <div class="container mt-0 mb-5">
+            <div class="d-flex justify-content-center row">
+                <div class="col-md-10 mt-5">
+                    <div class="row p-2 bg-white border rounded mt-2" v-for="(reservation, i) in Reservations" :key="i">
+                        <div class="col-md-3 col- mt-1">
+
+                            <div :id="generateIdSlider(i)" class="carousel slide" data-bs-ride="carousel">
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active" v-if="!(reservation.service.photos[0] === undefined)">
+                                        <img :src="reservation.service.photos[0].assetPath"
+                                            class="img-fluid img-responsive rounded product-image" alt="image">
+                                    </div>
+                                    <div v-for="(photo, i) in reservation.service.photos.slice(1)" :key="i"
+                                        class="carousel-item">
+                                        <img :src="photo.assetPath"
+                                            class="img-fluid img-responsive rounded product-image" alt="image">
+                                    </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button"
+                                    :data-bs-target="generateIdSliderWithHashTag(i)" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button"
+                                    :data-bs-target="generateIdSliderWithHashTag(i)" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
+
+                        </div>
+                        <div class="col-md-6 mt-1">
+                            <h5>{{ reservation.service.name }}</h5>
+                            <br />
+                            <p class="text-justify para mb-0">Datum početka rezervacije: {{
+                                    this.transformDates(reservation.startDate)
+                            }}</p>
+                            <p class="text-justify para mb-0">Datum kraja rezervacije: {{
+                                    this.transformDates(reservation.endDate)
+                            }}</p>
+                            <p class="text-justify para mb-0">Broj osoba: {{ reservation.capacity }}</p>
+                            <br />
+                            <p class="text-justify para mb-0">Adresa: {{ this.transformAddress(reservation.service.address) }}
+                            </p>
+                        </div>
+                        <div class="align-items-center align-content-center col-md-3 border-left pb-3">
+                            <div class="d-flex flex-row align-items-center">
+                                <h4 class="mr-1">${{ reservation.price }}</h4>
+                            </div>
+                            <div class="d-flex flex-column mt-2 pr-4">
+                                <!-- <star-rating rating="3"></star-rating> -->
+                                <star-rating v-model:rating="reservation.rating" :increment="0.01" :show-rating="false"
+                                    read-only></star-rating>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import StarRating from 'vue-star-rating';
+
+export default {
+    name: 'InstCompletedReservations',
+    data() {
+        return {
+            Reservations: [],
+            ReservationsEmpty: true,
+            ReservationsLoaded: false,
+
+        }
+    },
+    mounted() {
+        this.ReservationsLoaded = false;
+        this.loadReservations();
+        window.scrollTo(0, 0);
+    },
+    methods: {
+        async loadReservations() {
+            axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.$store.accessToken;
+            let self = this;
+            axios.get('/api/instructor/' + this.$store.User.id + '/get-completed-reservations')
+            .then((response) => {
+                self.Reservations = response.data;
+                self.ReservationsEmpty = self.Reservations.length === 0 ? true : false;
+                self.ReservationsLoaded = true;
+            }).catch((err) => {
+                alert('Doslo je do greske');
+                console.log(err);
+            })
+            
+        },
+        transformDates(date) {
+            //  2020, 5, 6, 0, 0
+            return date[2].toString() + '.' + date[1].toString() + '.' + date[0].toString() + '.';
+        },
+        transformAddress(address) {
+            return address.street + ', ' + address.city + ', ' + address.state;
+        },
+        generateIdSlider(id) {
+            return 'carouselExampleIndicators' + id;
+        },
+        generateIdSliderWithHashTag(id) {
+            return '#carouselExampleIndicators' + id;
+        },
+    },
+    components: {
+        StarRating: StarRating
+    }
+}
+</script>
+
+
+<style scoped>
+body {
+    background: #eee;
+}
+
+.product-image {
+    width: 100%
+}
+
+.dot {
+    height: 7px;
+    width: 7px;
+    margin-left: 6px;
+    margin-right: 6px;
+    margin-top: 3px;
+    background-color: blue;
+    border-radius: 50%;
+    display: inline-block
+}
+
+.spec-1 {
+    color: #938787;
+    font-size: 15px
+}
+
+h5 {
+    font-weight: 400
+}
+
+.para {
+    font-size: 16px
+}
+
+#nema-podataka {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    min-height: 100vh;
+    font-size: 28px;
+    color: royalblue;
+    background-color: white;
+}
+
+#ucitavanje {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    min-height: 100vh;
+    font-size: 28px;
+    color: royalblue;
+    background-color: white;
+}
+
+.pagination {
+    /* border: 1px solid red; */
+    width: 100%;
+    display: flex;
+    justify-content: center;
+}
+
+.form-control {
+    display: block;
+    width: 100%;
+    padding: 0.875rem 1.125rem;
+    font-size: 0.875rem;
+    font-weight: 400;
+    line-height: 1;
+    color: #69707a;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #c5ccd6;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    border-radius: 0.35rem;
+    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+</style>
+
+
